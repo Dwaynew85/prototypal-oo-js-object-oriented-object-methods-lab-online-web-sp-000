fe53fde42e2a91e7938ac0246e1867e45a84d4bf
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-15 18:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
 }
+
+User.prototype.veto = return "No, I must disagree";
+User.prototype.approve = return "You can do that!";
+User.prototype.doCharity = return "I like to help people.";
+User.prototype.releasePressStatement = return "You will see great things from Scuber.";
