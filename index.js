92f69d5692e8d088b6c85dbb08d5998156838bc4
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-15 18:25:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ User.prototype.veto = return "No, I must disagree";
 User.prototype.approve = return "You can do that!";
 User.prototype.doCharity = return "I like to help people.";
 User.prototype.releasePressStatement = return "You will see great things from Scuber.";
+User.prototype.sayHi = return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
